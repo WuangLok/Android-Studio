f12a9ea01766f8c19ccddc5de06526b7fb1a7e6c
Sakurachan1280 <Sakura12800@>
2024-07-19 10:23:36 +0700

    random

---
+++ app/src/main/java/com/example/finalproject/random/DishDisplayFragment.java
@@ -0,0 +1,58 @@
+package com.example.finalproject.random;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.finalproject.DBHandler.DBHelper;
+import com.example.finalproject.R;
+import com.example.finalproject.TimKiem.MonAn;
+
+public class DishDisplayFragment extends Fragment {
+
+    private TextView tvSuggestion;
+    private ImageView imgDish;
+    private TextView tvDishName;
+    private TextView tvDishType;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_random, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        tvSuggestion = view.findViewById(R.id.tvSuggestion);
+        imgDish = view.findViewById(R.id.imgHinhAnh);
+        tvDishName = view.findViewById(R.id.tvtenMonan);
+        tvDishType = view.findViewById(R.id.tvloaiMonan);
+
+        tvSuggestion.setText("Hôm nay ăn gì?");
+
+        DBHelper dbHelper = new DBHelper(getContext());
+        MonAn randomDish = dbHelper.getRandomMonan();
+
+        if (randomDish != null) {
+
+            tvDishName.setText(randomDish.getTenMonAn());
+            tvDishType.setText(randomDish.getLoaiMonAn());
+
+            int drawableId = getResources().getIdentifier(randomDish.getHinhAnh(), "drawable", getContext().getPackageName());
+            if (drawableId != 0) {
+                imgDish.setImageResource(drawableId);
+            } else {
+                imgDish.setImageResource(R.drawable.error);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/finalproject/random/RandomFragment.java
@@ -0,0 +1,60 @@
+package com.example.finalproject.random;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.finalproject.DBHandler.DBHelper;
+import com.example.finalproject.R;
+import com.example.finalproject.TimKiem.MonAn;
+
+public class RandomFragment extends Fragment {
+
+    private DBHelper dbHelper;
+
+    public RandomFragment() {
+
+    }
+
+    public static RandomFragment newInstance() {
+        return new RandomFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        dbHelper = new DBHelper(getContext());
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_random, container, false);
+
+        ImageView imageView = view.findViewById(R.id.imgMonan);
+        TextView nameTextView = view.findViewById(R.id.tvtenMonan);
+        TextView typeTextView = view.findViewById(R.id.tvloaiMonan);
+
+        MonAn randomMonAn = dbHelper.getRandomMonan();
+
+        if (randomMonAn != null) {
+            int resID = getContext().getResources().getIdentifier(randomMonAn.getHinhAnh(), "drawable", getContext().getPackageName());
+            imageView.setImageResource(resID);
+            nameTextView.setText(randomMonAn.getTenMonAn());
+            typeTextView.setText(randomMonAn.getLoaiMonAn());
+        } else {
+            nameTextView.setText("Không có món ăn nào");
+            typeTextView.setText("");
+            imageView.setImageResource(R.drawable.error);
+        }
+
+        return view;
+    }
+}
